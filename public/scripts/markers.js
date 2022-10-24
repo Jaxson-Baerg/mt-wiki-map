cc1b01683f2ca1bd45f3329575338920fadcf898
Sam <samjulian6@gmail.com>
2022-10-24 13:44:57 -0700

    Conflict resolution

--- public/scripts/markers.js
+++ public/scripts/markers.js
@@ -1,7 +1,9 @@
-$('select').on('change', this.val() , getMarkers);
-
 const getMarkers = (category) => {
   $.get('/markers', {category: category})
     .then(markers => {loadMarkers(markers)})
     .catch(err => {console.log(err)});
 };
+
+const $categorySelector = $('#category-selector');
+
+$categorySelector.on('change', $categorySelector.val() , getMarkers);
