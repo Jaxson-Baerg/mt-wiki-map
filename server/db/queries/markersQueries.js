9385b3dc93cca4c8b5abc37c43b3d2f3e6b7e29c
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-10-24 16:10:48 +0000

    Switch lat and lon to match google maps api params.

--- server/db/queries/markersQueries.js
+++ server/db/queries/markersQueries.js
@@ -2,7 +2,7 @@ const db = require('../connection').db;
 
 const addMarker = (marker) => {
   return db
-    .query(`INSERT INTO markers (longitude, latitude, thumbnail_photo_url, rating, title, description, user_id, public, category) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;`, [marker.longitude, marker.latitude, marker.thumbnail_photo_url, marker.rating, marker.title, marker.description, marker.user_id, marker.public, marker.category])
+    .query(`INSERT INTO markers (latitude, longitude, thumbnail_photo_url, rating, title, description, user_id, public, category) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;`, [marker.latitude, marker.longitude, marker.thumbnail_photo_url, marker.rating, marker.title, marker.description, marker.user_id, marker.public, marker.category])
     .then(result => {return Promise.resolve(result.rows)})
     .catch(err => {console.log(err)});
 };
