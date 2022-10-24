e48bfc8c4524f715e980c774e316eb2d71b34171
Jason Ling <Jasonling1996@gmail.com>
2022-10-24 17:33:45 +0000

    login,register,cookie

--- server/routes/login.js
+++ server/routes/login.js
@@ -15,9 +15,15 @@ const login = (email, password) => {
     });
 };
 
+// GET for login
+router.get('/', (req, res) => {
+   return res.render('login');
+});
+
 // POST for login
 router.post('/', (req, res) => {
   const {email, password} = req.body;
+  console.log("req.body",req.body);
   login(email, password)
     .then(user => {
       if (!user) {
@@ -26,7 +32,9 @@ router.post('/', (req, res) => {
       }
       console.log(user);
       req.session.userId = user.id;
-      res.send({user: {email: user.email, id: user.id}});
+      // res.send({user: {email: user.email, id: user.id}});
+      res.redirect('../');
+      return;
     })
     .catch(err => res.send(err));
 });
--- server/routes/register.js
+++ server/routes/register.js
@@ -3,6 +3,13 @@ const bcrypt = require('bcrypt');
 const database = require('../db/connection');
 const router  = express.Router();
 
+
+// GET for register
+router.get('/', (req, res) => {
+  return res.render('register');
+});
+
+// POST for register
 router.post('/', (req, res) => {
   const user = req.body;
   user.password = bcrypt.hashSync(user.password, 12);
@@ -15,9 +22,15 @@ router.post('/', (req, res) => {
 
       console.log(user);
       req.session.userId = user.id;
-      res.send({user: {user_id: user.id, user_email: user.email}});
+      // res.send({user: {user_id: user.id, user_email: user.email}});
+      res.redirect('../');
+      return;
     })
     .catch(err => res.send(err));
 });
 
 module.exports = router;
+//app.post("/logout", (req, res) => {
+//   req.session = null;
+//   return res.redirect("/urls");
+// });
