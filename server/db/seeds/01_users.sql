8405fc0264efb32fe5156369fcf11287f42b7243
Sam <samjulian6@gmail.com>
2022-10-25 13:37:17 -0700

    Add new favourites field to schema and data

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (email, password, favorite)
+INSERT INTO users (email, password, favourites)
   VALUES
     ('test1@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', ARRAY [1, 2, 3]),
     ('test2@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', ARRAY [2, 3]),
