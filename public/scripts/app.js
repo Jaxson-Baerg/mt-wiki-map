9ec1d9e7755ad84d1d672c1ac5d0dc982e7ee633
Sam <samjulian6@gmail.com>
2022-10-24 17:09:00 -0700

    Add functionality to load all markers on map based on category query

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,79 +2,20 @@
 
 // Array of markers that will be loaded on map
 const markerArr = [];
+let mapOptions, map;
 
 // Initialize and add the map
 const initMap = () => {
   // Locations of Canada and Vancouver
   const canada = { lat: 56.1304, lng: -106.3468 }
-  const vancouver = { lat: 49.2827, lng: -123.1207 };
 
-  const mapOptions = {
+  mapOptions = {
     zoom: 4,
     center: canada
   }
 
   // The map, centered at Canada
-  const map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-  // markers is an array of Marker objects to be placed on the map
-  const loadMarkers = markers => {
-    // Clear all pre-existing markers from map
-    markerArr.forEach(marker => marker.setMap(null))
-    markerArr.length = 0;
-
-    // Place each marker at given coords with an animation and title
-    markers.forEach(marker => {
-      const newMarker = new google.maps.Marker({
-        position: { lat: marker.latitude, lng: marker.longitude },
-        map: map,
-        animation: google.maps.Animation.DROP,
-        title: marker.title
-      })
-
-      // Add info window to each marker
-      google.maps.event.addListener(newMarker, "click", () => {
-        let infowindow = new google.maps.InfoWindow();
-        infowindow.setContent(`
-        <h1>${marker.title}</h1>
-        <div class="content">
-          <p>${marker.description}</p>
-          <p>Rating: ${marker.rating}/5</p>
-          <img src=${marker.thumbnail_photo_url}>
-        </div>
-        `);
-        infowindow.open(map, newMarker);
-     });
-
-     markerArr.push(marker);
-    });
-
-    // Cluster markers using marker clusterer
-    new markerClusterer.MarkerClusterer({
-      map,
-      markerArr
-    });
-  }
-
-  // The marker, positioned at Vancouver
-  const marker1 = new google.maps.Marker({
-    position: vancouver,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    draggable: true,
-    title:"Drag me!"
-  });
-
-  google.maps.event.addListener(marker1, "click", () => {
-    let infowindow = new google.maps.InfoWindow();
-    infowindow.setContent(`
-    <h1>Test</h1>
-    <div class="content">
-      <p>Highly reccomend coming here!</p>
-    </div>
-    `);
-    infowindow.open(map, marker1);
- })
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
   const placeMarkerAndPanTo = (latLng, map) => {
     new google.maps.Marker({
@@ -85,9 +26,6 @@ const initMap = () => {
   }
 
   map.addListener("click", e => placeMarkerAndPanTo(e.latLng, map));
-
 }
 
-
 window.initMap = initMap;
-
--- public/scripts/markers.js
+++ public/scripts/markers.js
@@ -1,9 +1,54 @@
+/// paste loadmarkers
+const loadMarkers = markers => {
+
+  // Clear all pre-existing markers from map
+  markerArr.forEach(marker => marker.setMap(null));
+  markerArr.length = 0;
+
+  // Place each marker at given coords with an animation and title
+  markers.forEach(marker => {
+    const newMarker = new google.maps.Marker({
+      position: { lat: marker.latitude, lng: marker.longitude },
+      map: map,
+      animation: google.maps.Animation.DROP,
+      title: marker.title
+    });
+
+    // Add info window to each marker
+    google.maps.event.addListener(newMarker, "click", () => {
+      let infowindow = new google.maps.InfoWindow();
+      infowindow.setContent(`
+      <h1>${marker.title}</h1>
+      <div class="content">
+        <p>${marker.address}</p>
+        <p>${marker.description}</p>
+        <p>Rating: ${marker.rating}/5</p>
+        <img src=${marker.thumbnail_photo_url}>
+      </div>
+      `);
+      infowindow.open(map, newMarker);
+   });
+
+   markerArr.push(newMarker);
+  });
+
+  // Cluster markers using marker clusterer
+  new markerClusterer.MarkerClusterer({
+    map,
+    markerArr
+  });
+}
+
 const getMarkers = (category) => {
   $.get('/markers', {category: category})
-    .then(markers => {loadMarkers(markers)})
+    .then(markers => loadMarkers(markers))
     .catch(err => {console.log(err)});
 };
 
-const $categorySelector = $('#category-selector');
+$( window ).on('load', () => {
+  getMarkers('all');
+});
 
-$categorySelector.on('change', $categorySelector.val() , getMarkers);
+$('.category-selector').change(function() {
+  getMarkers($(this).val());
+});
