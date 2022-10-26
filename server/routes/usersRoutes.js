58aa83688292017b5d078a854ccac91e0e83fcf2
Sam <samjulian6@gmail.com>
2022-10-26 12:55:51 -0700

    Update rendering of favourite marker information on profile

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -28,20 +28,28 @@ router.get('/create', (req, res) => {
   res.render('createMarkers', templateVar);
 });
 
+router.get('/favourites', (req, res) => {
+  markersQueries.getFavouriteMarkersById(req.session.userId)
+    .then((favArr) => {
+      res.send({favArr: favArr, loggedIn: req.session.userId});
+    })
+    .catch(err => {console.log(err)});
+});
+
 const helper = (favourites, favouritesArr) => {
   return new Promise((resolve, reject) => {
-    favourites.forEach(favourite => {
+    favourites[0].favourites.forEach(favourite => {
       markersQueries.getMarkerById(favourite)
-      .then(fav => {
-        favouritesArr.push(fav);
-        if (favourites.indexOf(favourite) === favourites.length - 1) {
-          resolve(favouritesArr);
-        }
-      })
-      .catch(err => res.send(err));
-    })
-  })
-}
+        .then(fav => {
+          favouritesArr.push(fav);
+          if (favourites[0].favourites.indexOf(favourite) === favourites[0].favourites.length - 1) {
+            resolve(favouritesArr);
+          }
+        })
+        .catch(err => res.send(err));
+    });
+  });
+};
 
 router.get('/profile', (req, res) => {
   let userId = req.session.userId;
