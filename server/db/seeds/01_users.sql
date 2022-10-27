66f88fb1f55a6668d61b3ed0dfb33100581666a9
Jason Ling <Jasonling1996@gmail.com>
2022-10-27 21:07:25 +0000

    Added more seeds

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO users (email, password, favourites)
   VALUES
-    ('test1@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', ARRAY [1, 2, 3]),
+    ('test1@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', ARRAY [1, 3, 5]),
     ('test2@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', ARRAY [2, 3]),
     ('test3@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', ARRAY [1, 3]);
